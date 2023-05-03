2e197aa3ad90f58c6a9cec680dbe084594f4270a
AntoniaEkdahl <antoniaekdahl@gmail.com>
2023-05-03 11:41:56 +0200

    Remove copy of dropdown component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Fragment } from "react";
 import "./App.css";
 import Hero from "./Hero";
-import DropdownMenu from "./DropdownMenu.jsx";
 import Header from "./Header";
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <DropdownMenu />
       <Hero />
     </Fragment>
   );
