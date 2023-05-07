748114b804fb4d37084b26d146089c8eb592502d
marcus therup <m_therup@hotmail.com>
2023-05-07 21:00:36 +0200

    Product page done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,14 +3,16 @@ import "../css/ProductCard.css";
 
 function ProductCard({ product }) {
   return (
-    <Card>
-      <Card.Img variant="top" src={product.bild} alt={product.produktnamn} />
-      <Card.Body>
-        <Card.Title>{product.produktnamn}</Card.Title>
-        <Card.Text>{product.kategori}</Card.Text>
-        <Card.Text>{product.info}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card>
+        <Card.Img variant="top" src={product.bild} alt={product.produktnamn} />
+        <Card.Body>
+          <Card.Title>{product.produktnamn}</Card.Title>
+          <Card.Text>{product.kategori}</Card.Text>
+          <Card.Text>{product.info}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
